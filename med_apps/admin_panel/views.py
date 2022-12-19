25b52307e538feec0d7bf97728040adfa2c92a12
sarvar raxmonov <tturkbola@gmail.com>
2022-12-19 16:48:04 +0500

    Med api

--- med_apps/admin_panel/views.py
+++ med_apps/admin_panel/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from rest_framework.response import Response
+from med_apps.homiy.models import HomiyArizasi
+from med_apps.talaba.models import Talaba_qushish
+from django.db.models import Sum
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from django.db.models import Count
 # Create your views here.
+
+
+
+class AdminPanel(ReadOnlyModelViewSet):
+
+      queryset = HomiyArizasi.objects.all()
+      def list(self,request):
+            
+          homiylar_tulagan_summa = HomiyArizasi.objects.aggregate(Sum('Sarflangan_summa'))
+          talabalar_jammi_kontrakti = Talaba_qushish.objects.aggregate(Sum('Kontrakt_summa'))
+          talablarga_tulanishi_kerak_summa =  abs(talabalar_jammi_kontrakti.get('Kontrakt_summa__sum') - homiylar_tulagan_summa.get('Sarflangan_summa__sum'))
+
+         
+          return Response({'Tulangan summa':homiylar_tulagan_summa,'Ummumiy suralgan summa':talabalar_jammi_kontrakti.get('Kontrakt_summa__sum'),
+                           'Tulanishi kerak bulgan summa':talablarga_tulanishi_kerak_summa})
+         
--- med_apps/urls.py
+++ med_apps/urls.py
@@ -2,6 +2,7 @@ from rest_framework import routers
 from django.urls import path, include
 from .homiy.views import Homiy_arizasi, Homiy_all_data_for_filtering
 from .talaba.views import Talaba_qushish_view
+from .admin_panel.views import AdminPanel
 router = routers.DefaultRouter()
 app_name = 'myapp'
 
@@ -9,6 +10,7 @@ app_name = 'myapp'
 router.register('ariza',Homiy_arizasi,basename='ariza')
 router.register('talaba',Talaba_qushish_view,basename='talaba')
 router.register('homiy_all_data',Homiy_all_data_for_filtering,basename='homiy_all_data')
+router.register('admin_panel',AdminPanel,basename='admin_panel')
 
 urlpatterns = [
     path('ariza/<pk>/',Homiy_arizasi.as_view({'get': 'retrieve'}),name='ariza-detail'),
